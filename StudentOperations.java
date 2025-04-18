e9d8be70c685adf1c899e4494de7d9b73597acbe
unknown <vidish162005@gmail.com>
2025-04-19 01:00:22 +0530

    modified Student Operations.java

--- StudentOperations.java
+++ StudentOperations.java
@@ -9,5 +9,12 @@ class StudentOperations {
         students = new ArrayList<>();
         scan = new Scanner(System.in);
     }
-
+    //method to add student with exception 
+    public void addStudent(Student student) throws InvalidStudentDataException {
+        if (student == null) {
+            throw new InvalidStudentDataException("Student data cannot be null.");
+        }
+        students.add(student);
+        System.out.println("Student added successfully.");
+    }
 }
