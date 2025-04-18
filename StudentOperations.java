90eac62df8e7d92392e8c3be1d11e8734fef651a
unknown <vidish162005@gmail.com>
2025-04-19 00:59:34 +0530

    modified Student Operations.java

---
+++ StudentOperations.java
@@ -0,0 +1,13 @@
+mport java.util.ArrayList;
+import java.util.Scanner;
+
+class StudentOperations {
+    private ArrayList<Student> students;
+    private Scanner scan;
+
+    public StudentOperations() {
+        students = new ArrayList<>();
+        scan = new Scanner(System.in);
+    }
+
+}
